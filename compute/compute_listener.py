37a4c76c4c6f77d0260e8c63b6830cbe1b505158
Ubuntu <ubuntu@ip-172-31-44-202.ec2.internal>
2014-11-24 04:59:51 +0000

    compute_listener.py now can receive a matrix in the form of a CSV, parse it, and return its inverse

--- compute/compute_listener.py
+++ compute/compute_listener.py
@@ -3,23 +3,46 @@ import time
 import requests 
 import simplejson as json
 import redis
+import numpy
 
 port = "80"
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:%s" % port)
 
+rowlist = []
+
 while True:
     msg = socket.recv()
+
+    for line in msg.split('\n'):
+        
+        if line:
+            # Have to 
+            #    1. Parse elements from each row
+            #    2. Remove the semicolon at the end of the line
+            #    3. Convert the elements to floats
+            row = map(float,line.replace(';','').split(','))
+            rowlist.append(row)
     
-    print msg
+    print rowlist
     
-    redisEndpoint = ''
-    redisPort = 6379
+    arr = numpy.vstack(rowlist)
     
-    r = redis.StrictRedis(host=redisEndpoint, port=redisPort)
+    print arr.shape
     
-    val = str(r.keys())
+    inv = numpy.linalg.inv(arr)
+     
+    print "---"
+     
+    print inv
+    
+#     redisEndpoint = ''
+#     redisPort = 6379
+#     
+#     r = redis.StrictRedis(host=redisEndpoint, port=redisPort)
+#     
+#     val = str(r.keys())
     
     socket.send(msg)
 
