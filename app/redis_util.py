7ba226d55e40665d12b9e52438fc8c93be2b0ffb
John Nielson <mr.john.nielson@gmail.com>
2014-12-05 19:36:48 +0000

    added redis_util.py for real this time

---
+++ app/redis_util.py
@@ -0,0 +1,9 @@
+import redis
+
+class redis_util():
+    
+    def __init__(self, redis_endpoint, port_num=6379):
+        
+        self.r = redis.StrictRedis(host=redis_endpoint, port=port_num, db=0)
+        
+        
