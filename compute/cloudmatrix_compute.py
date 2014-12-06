2a57c2c6fad4301dfdac20edb0744bb64869ad29
Ubuntu <ubuntu@ip-172-31-44-202.ec2.internal>
2014-12-06 20:54:44 +0000

    added cloudmatrix_compute.py

---
+++ compute/cloudmatrix_compute.py
@@ -0,0 +1,18 @@
+import numpy
+import redis
+
+import redis_util
+
+class Computer():
+
+    def __init__(self, ip):
+        self.ip = ip
+        self.ru = 
+    def add(self, operand_list):
+        
+        print "Computer.add.ip          : %s" % self.ip
+        print "Computer.add.operand_list: %s" % operand_list
+    
+    
+    
+    
